b7e78262e91523625bcecf2ec067b6fa7d749f03
nizarion <nizaralmesry@hotmail.com>
2018-09-26 21:39:25 +0100

    Automatic coloring in game_data

--- src/sc2convnet1.py
+++ src/sc2convnet1.py
@@ -1,8 +1,9 @@
-import sc2, random
-from sc2 import run_game, maps, Race, Difficulty
+import sc2, random, cv2, numpy as np
+from sc2 import run_game, maps, Race, Difficulty, game_data
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
-CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, ROBOTICSFACILITY, IMMORTAL
+CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, ROBOTICSFACILITY, IMMORTAL, \
+OBSERVER
 from  examples.protoss.cannon_rush import CannonRushBot
 from examples.terran.proxy_rax import ProxyRaxBot
 
@@ -14,6 +15,7 @@ class SentdeBot(sc2.BotAI):
     def __init__(self):
         self.ITERATIONS_PER_MINUTE = 165
         self.MAX_WORKERS = 70
+        self.draw_dict = {}
 
     async def on_step(self, iteration):
         self.iteration = iteration  # give access everywhere in here
@@ -26,6 +28,7 @@ class SentdeBot(sc2.BotAI):
         await self.expand()  # expand to a new resource area.
         await self.offensive_force_buildings()
         await self.build_offensive_force()
+        await self.intel()
         await self.attack()
 
     async def build_workers(self):
@@ -90,17 +93,71 @@ class SentdeBot(sc2.BotAI):
             return random.choice(self.known_enemy_structures)
         else:
             return self.enemy_start_locations[0]
+    
+    async def intel(self):
+        # for game_info: https://github.com/Dentosal/python-sc2/blob/master/sc2/game_info.py#L162
+        # print(self.game_info.map_size)
+        # flip around. It's y, x when you're dealing with an array.
+        # print(dir(self.units(NEXUS)))
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
+        
+        self.draw_circle(game_data, 0)
+        self.draw_circle(game_data, 1)
+#         for unit in self.units().ready:
+#             name = unit.name.lower()
+#             id = unit.type_id.value
+#             if not name in self.draw_dict:
+#                 self.draw_dict[name] = [int(unit.radius * 4), (random.randint(0, 255), random.randint(0, 255), 0)]  # random.randint(0, 5) (id % 255, id // 255 * 32, 0)
+#             pos = unit.position
+#             # print(pos)
+#             cv2.circle(game_data, (int(pos[0]), int(pos[1])), self.draw_dict[name][0], self.draw_dict[name][1], -1)  # BGR 
+#                
+#         for unit in self.known_enemy_units:
+#             name = unit.name.lower()
+#             id = unit.type_id.value
+#             if not name in self.draw_dict:
+#                 self.draw_dict[name] = [int(unit.radius * 4), (random.randint(0, 255), random.randint(0, 255), 255)]  # random.randint(0, 5) (id % 255, id // 255 * 32, 0)
+#             pos = unit.position
+#             # print(pos)
+#             cv2.circle(game_data, (int(pos[0]), int(pos[1])), self.draw_dict[name][0], self.draw_dict[name][1], -1)  # BGR 
+            
+        # flip horizontally to make our final fix in visual representation:
+        flipped = cv2.flip(game_data, 0)
+        resized = cv2.resize(flipped, dsize=None, fx=2, fy=2)
+
+        cv2.imshow('Intel', resized)
+        cv2.waitKey(1)
 
     async def attack(self):
         # {UNIT: [n to fight, n to defend]}
         aggressive_units = {VOIDRAY: [8, 3]}
-
         for UNIT in aggressive_units:
-            for s in self.units(UNIT).idle:
-                await self.do(s.attack(self.find_target(self.state)))
+            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+
+            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
+                
+    def draw_circle(self, game_data, is_enemy):
+        if is_enemy:
+            red_colour = 255
+        else:
+            red_colour = 0
+            
+        for unit in (self.known_enemy_units if is_enemy else self.units().ready):
+            name = unit.name.lower()
+            id = unit.type_id.value
+            if not name in self.draw_dict:
+                self.draw_dict[name] = [int(unit.radius * 4), (random.randint(0, 255), random.randint(0, 255), red_colour)]  # random.randint(0, 5) (id % 255, id // 255 * 32, 0)
+            pos = unit.position
+            # print(pos)
+            cv2.circle(game_data, (int(pos[0]), int(pos[1])), self.draw_dict[name][0], self.draw_dict[name][1], -1)  # BGR 
 
 
 run_game(maps.get("(2)CatalystLE"), [
     Bot(Race.Protoss, SentdeBot()),
-    Computer(Race.Zerg, Difficulty.Hard)
+    Computer(Race.Terran, Difficulty.Harder)
 ], realtime=False)
